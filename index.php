e2ac4cf339970157d56eb59146a772ddb8f9efdc
RuslanMitnovitskij <125449053+RuslanMitnovitskij@users.noreply.github.com>
2023-05-31 10:20:46 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php 
+
+echo "<a href='insertKlant.php'>Voeg nieuwe klant toe</a></br>";
+
+echo "<a href='insertVerkoop.php'>Maak nieuwe verkooporder aan</a></br>";
+
+echo "<a href='selectVerkoop.php'>laat alle verkooporders zien</a></br>";
+
+
+?>
